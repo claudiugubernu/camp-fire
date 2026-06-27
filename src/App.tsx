b8b8a09d9ff425c26b63eb4a0fcff6a0ad05e6f0
Claudiu Gubernu <claudiugubernu@gmail.com>
2026-06-27 11:11:15 +0300

    Making sure you're re-directed to onboarding if the page you're trying to get to doesn't exist

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ export default function App() {
             <Navigate
               to='/onboarding'
               replace
+              state={{ from: location.pathname }}
             />
           }
         />
